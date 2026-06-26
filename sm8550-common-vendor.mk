fc1d1fc32cbc27acae9ae063ae430d03f97836ae
krazey <admin@krazey.de>
2026-06-26 16:05:52 +0200

    sm8550-common oss audio
    Change-Id: I877d7e4283756b0e2f2fb5f17b0a35cb2b2f1a59

--- sm8550-common-vendor.mk
+++ sm8550-common-vendor.mk
@@ -758,7 +758,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.data.qmi@1.0 \
     vendor.qti.hardware.dsp@1.0 \
     vendor.qti.hardware.mwqemadapter@1.0 \
-    vendor.qti.hardware.pal@1.0-impl \
     vendor.qti.hardware.qccsyshal@1.0 \
     vendor.qti.hardware.qccsyshal@1.1 \
     vendor.qti.hardware.qccsyshal@1.2 \
